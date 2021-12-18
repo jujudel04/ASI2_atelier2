bd55629acaca5ad8e95bb41a88accd3562d15feb
Justin Ravilly <78474775+justin-ravilly@users.noreply.github.com>
2021-12-18 15:04:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,49 @@
-var app = require("express")();
+var express = require("express"); 
+var app = express();
+app.use(express.static('public'));
 var http = require("http").createServer(app);
+var https = require("https");
 var io = require("socket.io")(http);
-const {joinUser, removeUser, findUser} = require('./users');
+const {joinUser, removeUser} = require('./users');
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 let thisRoom = "";
+var users_list=[];
+var users_count=0;
+
 io.on("connection", function (socket) {
-  console.log("connected");
+  //Création du chat room
+  console.log("Connexion supplémentaire detectée : "+socket.id);
+  //Code executé lorsque l'on rejoint un chat room
+  socket.on("Server Connection", (data) => {
+    //users_list[users_count]=data;
+    users_list.push(data.username);
+    users_count+=1;
+    console.log("Liste actuelle des utilisateurs : "+users_list);
+    console.log("Taille liste actuelle des utilisateurs : "+users_list.length);
+    io.emit("user_list_sent",users_list);
+  });
+  socket.on("connection_to_user", (data_sent) => {
+      console.log("Connexion entre "+data_sent.user_to_connect+" et "+data_sent.actual_user+" en cours");
+      thisRoom=data_sent.user_to_connect+"_"+data_sent.actual_user;
+      //console.log("Liste des sockets "+io.sockets.sockets.size);
+      io.emit("connection_time", {
+        first_user: data_sent.actual_user,
+        second_user: data_sent.user_to_connect 
+      })
+  });
   socket.on("join room", (data) => {
-    console.log('in room');
-    let Newuser = joinUser(socket.id, data.username,data.roomName)
+    console.log('in room : '+ thisRoom);
+    let Newuser = joinUser(socket.id, data.username,thisRoom)
     //io.to(Newuser.roomname).emit('send data' , {username : Newuser.username,roomname : Newuser.roomname, id : socket.id})
    // io.to(socket.id).emit('send data' , {id : socket.id ,username:Newuser.username, roomname : Newuser.roomname });
    socket.emit('send data' , {id : socket.id ,username:Newuser.username, roomname : Newuser.roomname });
-   
-    thisRoom = Newuser.roomname;
     console.log(Newuser);
     socket.join(Newuser.roomname);
   });
   socket.on("chat message", (data) => {
-    io.to(thisRoom).emit("chat message", {data:data,id : socket.id});
+    io.to(thisRoom).emit("chat message", {data:data, id : socket.id});
   });
   socket.on("disconnect", () => {
     const user = removeUser(socket.id);
@@ -32,5 +55,4 @@ io.on("connection", function (socket) {
 
   });
 });
-
 http.listen(3005, function () {});
